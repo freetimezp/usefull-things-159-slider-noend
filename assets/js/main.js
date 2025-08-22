94c5d1e007eead1ba8536da948707eddd0703abb
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-08-22 17:40:22 +0300

    04. usefull-things-159-slider-noend. Opacity.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "Luminius",
         "Serenity",
         "Nebula Point",
-        "Horizon"
+        "Horizon",
     ];
 
     const slideDescriptions = [
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "Provocated",
         "Music Vol",
         "VFX",
-        "Set Illusion"
+        "Set Illusion",
     ];
 
     function createSlide(slideNumber, direction) {
@@ -34,11 +34,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const img = document.createElement("img");
         img.src = `./assets/images/img${slideNumber}.jpg`;
-        img.alt = '';
+        img.alt = "";
 
         slideBgImg.appendChild(img);
         slide.appendChild(slideBgImg);
-
 
         if (direction === "down") {
             slideBgImg.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
@@ -49,14 +48,13 @@ document.addEventListener("DOMContentLoaded", () => {
         return slide;
     }
 
-
     function createMainImageWrapper(slideNumber, direction) {
         const wrapper = document.createElement("div");
         wrapper.className = "slide-main-img-wrapper";
 
         const img = document.createElement("img");
         img.src = `./assets/images/img${slideNumber}.jpg`;
-        img.alt = '';
+        img.alt = "";
 
         wrapper.appendChild(img);
 
@@ -69,29 +67,27 @@ document.addEventListener("DOMContentLoaded", () => {
         return wrapper;
     }
 
-
     function createTextElements(slideNumber, direction) {
         const newTitle = document.createElement("h1");
         newTitle.textContent = slideTitles[slideNumber - 1];
         gsap.set(newTitle, {
-            y: direction === "down" ? 50 : -50
+            y: direction === "down" ? 50 : -50,
         });
 
         const newDescription = document.createElement("p");
         newDescription.textContent = slideDescriptions[slideNumber - 1];
         gsap.set(newDescription, {
-            y: direction === "down" ? 20 : -20
+            y: direction === "down" ? 20 : -20,
         });
 
         const newCounter = document.createElement("p");
         newCounter.textContent = slideNumber;
         gsap.set(newCounter, {
-            y: direction === "down" ? 18 : -18
+            y: direction === "down" ? 18 : -18,
         });
 
         return { newTitle, newDescription, newCounter };
     }
-
 
     function animateSlide(direction) {
         if (isAnimating || !scrollAllowed) return;
@@ -129,19 +125,14 @@ document.addEventListener("DOMContentLoaded", () => {
         counterContainer.appendChild(newCounter);
 
         gsap.set(newMainWrapper.querySelector("img"), {
-            y: direction === "down" ? "-50%" : "50%"
+            y: direction === "down" ? "-50%" : "50%",
         });
-
 
         const tl = gsap.timeline({
             onComplete: () => {
-                [
-                    currentSlideElement,
-                    currentMainWrapper,
-                    currentTitle,
-                    currentDescription,
-                    currentCounter
-                ].forEach((el) => el?.remove());
+                [currentSlideElement, currentMainWrapper, currentTitle, currentDescription, currentCounter].forEach(
+                    (el) => el?.remove()
+                );
 
                 isAnimating = false;
 
@@ -149,72 +140,116 @@ document.addEventListener("DOMContentLoaded", () => {
                     scrollAllowed = true;
                     lastScrollTime = Date.now();
                 }, 100);
-            }
+            },
         });
 
-
-        tl
-            .to(newSlide.querySelector(".slide-bg-img"), {
-                clipPath: direction === "down"
-                    ? "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"
-                    : "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        tl.to(
+            newSlide.querySelector(".slide-bg-img"),
+            {
+                clipPath:
+                    direction === "down"
+                        ? "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"
+                        : "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
                 duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(currentSlideElement.querySelector("img"), {
-                scale: 1.5,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(newMainWrapper, {
-                clipPath: direction === "down"
-                    ? "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
-                    : "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(currentMainWrapper.querySelector("img"), {
-                y: direction === "down" ? "50%" : "-50%",
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(newMainWrapper.querySelector("img"), {
-                y: 0,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(currentTitle, {
-                y: direction === "down" ? -50 : 50,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(newTitle, {
-                y: 0,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(currentDescription, {
-                y: direction === "down" ? -20 : 20,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(newDescription, {
-                y: 0,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(currentCounter, {
-                y: direction === "down" ? -18 : 18,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0)
-            .to(newCounter, {
-                y: 0,
-                duration: 1.25,
-                ease: CustomEase.create("", "0.87, 0, 0.13, 1")
-            }, 0);
+                ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+            },
+            0
+        )
+            .to(
+                currentSlideElement.querySelector("img"),
+                {
+                    scale: 1.5,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                newMainWrapper,
+                {
+                    clipPath:
+                        direction === "down"
+                            ? "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+                            : "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                currentMainWrapper.querySelector("img"),
+                {
+                    y: direction === "down" ? "50%" : "-50%",
+                    duration: 1.25,
+                    opacity: 0.7,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                newMainWrapper.querySelector("img"),
+                {
+                    y: 0,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                currentTitle,
+                {
+                    y: direction === "down" ? -50 : 50,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                newTitle,
+                {
+                    y: 0,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                currentDescription,
+                {
+                    y: direction === "down" ? -20 : 20,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                newDescription,
+                {
+                    y: 0,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                currentCounter,
+                {
+                    y: direction === "down" ? -18 : 18,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            )
+            .to(
+                newCounter,
+                {
+                    y: 0,
+                    duration: 1.25,
+                    ease: CustomEase.create("", "0.87, 0, 0.13, 1"),
+                },
+                0
+            );
     }
-
 
     function handleScroll(direction) {
         const now = Date.now();
@@ -226,68 +261,54 @@ document.addEventListener("DOMContentLoaded", () => {
         animateSlide(direction);
     }
 
+    //scroll event
+    window.addEventListener(
+        "wheel",
+        (e) => {
+            e.preventDefault();
 
-    window.addEventListener("wheel", (e) => {
-        e.preventDefault();
+            const direction = e.deltaY > 0 ? "down" : "up";
+            handleScroll(direction);
+        },
+        { passive: false }
+    );
 
-        const direction = e.deltaY > 0 ? "down" : "up";
-        handleScroll(direction);
-    }, { passive: false });
-
+    //touch event
     let touchStartY = 0;
     let isTouchActive = false;
 
-    window.addEventListener("touchstart", (e) => {
-        e.preventDefault();
+    window.addEventListener(
+        "touchstart",
+        (e) => {
+            e.preventDefault();
 
-        const touchStartY = e.touches[0].clientY;
-        isTouchActive = true;
-    }, { passive: false });
+            const touchStartY = e.touches[0].clientY;
+            isTouchActive = true;
+        },
+        { passive: false }
+    );
 
-    window.addEventListener("touchmove", (e) => {
-        e.preventDefault();
+    window.addEventListener(
+        "touchmove",
+        (e) => {
+            e.preventDefault();
 
-        if (!isTouchActive || isAnimating || !scrollAllowed) return;
+            if (!isTouchActive || isAnimating || !scrollAllowed) return;
 
-        const touchCurrentY = e.touches[0].clientY;
-        const difference = touchStartY - touchCurrentY;
+            const touchCurrentY = e.touches[0].clientY;
+            const difference = touchStartY - touchCurrentY;
 
-        if (Math.abs(difference) > 10) {
-            isTouchActive = false;
+            if (Math.abs(difference) > 10) {
+                isTouchActive = false;
 
-            const direction = difference > 0 ? "down" : "up";
-            handleScroll(direction);
-        }
-    }, { passive: false });
+                const direction = difference > 0 ? "down" : "up";
+                handleScroll(direction);
+            }
+        },
+        { passive: false }
+    );
 
     window.addEventListener("touchend", () => {
         isTouchActive = false;
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
